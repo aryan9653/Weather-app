9403faa6f77d49471e4e1b28199ee936439eaa22
Aryan9653 <aryanshukla9004@gmail.com>
2024-06-14 11:30:08 +0530

    react-project

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = 'dfc129bfd1f5fbcb32607e2751910694';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
